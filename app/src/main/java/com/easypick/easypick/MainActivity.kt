4e8ffdbd88ce73bd7f064e01c71510ef01221e12
Lucas Gilone <lgilone@devartis.com>
2020-04-29 23:42:36 -0300

    Add initial look for app

--- app/src/main/java/com/easypick/easypick/MainActivity.kt
+++ app/src/main/java/com/easypick/easypick/MainActivity.kt
@@ -2,11 +2,25 @@ package com.easypick.easypick
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.easypick.easypick.fragments.MainFragment
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), MainFragment.OnFragmentInteractionListener{
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragmentContainer, MainFragment.newInstance())
+                .commit()
+        }
+    }
+
+    override fun showFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .addToBackStack(null)
+            .replace(R.id.fragmentContainer, fragment)
+            .commit()
     }
 }
---
+++ app/src/main/java/com/easypick/easypick/fragments/MainFragment.kt
@@ -0,0 +1,67 @@
+package com.easypick.easypick.fragments
+
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.easypick.easypick.R
+import kotlinx.android.synthetic.main.main_fragment.view.*
+
+class MainFragment: Fragment() {
+    private var listener: OnFragmentInteractionListener? = null
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.main_fragment, container, false)
+    }
+
+//    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+//        super.onViewCreated(view, savedInstanceState)
+//        view.changeScreenButton.setOnClickListener {
+//            listener?.showFragment(RegistrationFragment()) // Ejemplo, poner el fragment necesario
+//        }
+//    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if (context is OnFragmentInteractionListener) {
+            listener = context
+        } else {
+            throw RuntimeException("$context must implement OnFragmentInteractionListener")
+        }
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        listener = null
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     *
+     *
+     * See the Android Training lesson [Communicating with Other Fragments]
+     * (http://developer.android.com/training/basics/fragments/communicating.html)
+     * for more information.
+     */
+    interface OnFragmentInteractionListener {
+        fun showFragment(fragment: Fragment)
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @return A new instance of fragment MainFragment.
+         */
+        @JvmStatic
+        fun newInstance() = MainFragment()
+    }
+}
